77e1c1f8c7db7990d7bbc6109b1a9b326cab075b
WEI Zhicheng <zhicheng@opensourceforge.net>
2013-01-01 11:17:29 +0800

    change 'char' to 'signed char'

--- geohash.c
+++ geohash.c
@@ -13,8 +13,8 @@ static char base32en[] = {
 	's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 };
 
-/* ASCII order for BASE 32,ignore useless character */
-static char base32de[] = {
+/* ASCII order for BASE 32 decode,from '0' to 'z', -1 in unused character */
+static signed char base32de[] = {
 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
 	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
 	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
